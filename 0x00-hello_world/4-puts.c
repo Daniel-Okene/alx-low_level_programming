cfc521e46a191aa2cdbce2f4ca65f362ee516963
Daniel-Okene <simplydaniel11@gmail.com>
2023-02-16 15:50:11 -0800

    solution for task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - prints out a string using puts function
+ *
+ */
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
