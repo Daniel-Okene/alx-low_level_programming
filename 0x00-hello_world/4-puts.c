0b2447578176fd3d1091fbf522e80ac6846732df
Daniel-Okene <simplydaniel11@gmail.com>
2023-02-16 16:06:07 -0800

    fixed betty error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /*
- * main: prints out a string using puts function
+ * main - prints out a string using puts function
  *
+ * Return: 0 always on success
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /*
- * main: prints a string with the printf function
+ * main - prints a string with the printf function
  *
+ * Return: 0 always on success
  */
 
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
