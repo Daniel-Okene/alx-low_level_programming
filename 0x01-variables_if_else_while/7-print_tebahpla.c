81aa04c67a2fad0cb3eef785ffeeb97c3f1228ff
Daniel-Okene <simplydaniel11@gmail.com>
2023-02-17 17:22:07 -0800

    task solution for 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabelts in reverse
+ * followed by a new line
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i = 122;
+
+	while (i > 96)
+		putchar(i++);
+	putchar('\n');
+	return (0);
+}
+
