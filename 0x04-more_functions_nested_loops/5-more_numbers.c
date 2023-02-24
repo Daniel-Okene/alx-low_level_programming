eeb11eb3cfc6f4f179a16d1f40b6b9415bc684b8
Daniel-Okene <simplydaniel11@gmail.com>
2023-02-23 16:30:46 -0800

    made hanges to task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,28 @@
 #include "main.h"
 
 /**
- * more_numbers -  prints 10 times the numbers, from 0 to 14, followed by a new line
+ * more_numbers -  prints 10 times the numbers, from 0 to 14
+ * followed by a new line
  *
  * Return: void
  */
 
 void more_numbers(void)
 {
-	int i = 0;
+	int i;
+	int count = 0;
 
-	while (i < 15)
+	for (count < 10)
 	{
-		if (i > 9)
-			_putchar((i / 10) + '0');
-		_putchar((i % 10) + '0');
-		i++;
+		i = 0;
+		while (i < 15)
+		{
+			if (i > 9)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			i++;
+		}
+		count++;
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
