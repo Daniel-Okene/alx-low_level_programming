d28af6f59b59a63d4aa9a180efdc1719483341cd
Daniel-Okene <simplydaniel11@gmail.com>
2023-02-23 18:24:40 -0800

    solution for task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,7 +19,7 @@ int main(void)
 		if (i % 3 == 0 && i % 5 == 0)
 			printf(" FizzBuzz");
 		else if (i % 3 == 0)
-			printf(" FIzz");
+			printf(" Fizz");
 		else if (i % 5 == 0)
 			printf(" Buzz");
 		else if (i == 1)
