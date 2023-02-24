c31c91029486b84e08c8741ac3c0d8895895e982
Daniel-Okene <simplydaniel11@gmail.com>
2023-02-23 17:05:31 -0800

    solution for task 7 and few change to task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -17,5 +17,5 @@ void print_line(int n)
 			n--;
 		}
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times the character \ should be printed
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i = 1;
+	int j;
+
+	if (n > 0)
+	{
+		while (i <= n)
+		{
+			j = i;
+			while (j)
+			{
+				_putchar(' ');
+				j--;
+			}
+			i++;
+			_putchar('\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
+
+			
