a2774f3c41e72c0e3ee47d879767b41b172ec4a2
Daniel-Okene <simplydaniel11@gmail.com>
2023-03-02 18:15:37 -0800

    made a complete modification of task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,17 +10,24 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int count1 = 0;
-	int count2 = 0;
+	int i = 0;
+	int value;
 
-	while (s1[count1])
-		count1++;
-	while (s2[count2])
-		count2++;
-
-	if (count1 < count2)
-		return (-15);
-	else if (count1 > count2)
-		return (15);
-	return (0);
+	while (s1[1] && s2[1])
+	{
+		if (s1[i] > s2[i])
+		{
+			value = 15;
+			break;
+		}
+		else if (s1[i] < s2[i])
+		{
+			value = -15;
+			break;
+		}
+		else
+			value = 0;
+		i++;
+	}
+	return (value);
 }
