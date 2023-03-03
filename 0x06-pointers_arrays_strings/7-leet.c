7545746f7608137f60505eeb2003c1461fb68f10
Daniel-Okene <simplydaniel11@gmail.com>
2023-03-02 19:18:52 -0800

    solution for task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string to encode
+ *
+ * Return: pointer to string
+ */
+
+char *leet(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char letters[6] = {'a', 'e', 'o', 't', 'l'};
+	int values[6] = {4, 3, 0, 7, 1};
+
+	while (s[i])
+	{
+		while (letters[j])
+		{
+			if (s[i] == letters[j] || s[i] == letters[j] - 32)
+			{
+				s[i++] = values[j] + 48;
+				break;
+			}
+		}
+	}
+	return (s);
+}
