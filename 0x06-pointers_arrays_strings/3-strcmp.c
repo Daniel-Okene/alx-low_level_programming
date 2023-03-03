2cea4ce631d605a4a5e9e9ce0ad60c6a64ba265a
Daniel-Okene <simplydaniel11@gmail.com>
2023-03-02 18:02:11 -0800

    solution for task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,24 +10,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
-	int count = 0;
+	int i, count = 0;
 
 	for (i = 0; s1[i]; i++)
 	{
-		for (j = 0; s2[j]; j++)
-		{
-			if (s1[i] == s2[j])
-			{
-				count++;
-				break;
-			}
-		}
+		if (s1[i] == s2[i])
+			count++;
 	}
 
 	if (count < i)
-		return (-1);
+		return (-15);
 	else if (count > i)
-		return (1);
+		return (15);
 	return (0);
 
