120035eeb02e29683ffa633b7dc10e3e33a03df7
Daniel-Okene <simplydaniel11@gmail.com>
2023-03-02 17:42:39 -0800

    solution to task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,11 +18,10 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = src[i];
 		i++;
 	}
-
-	if (src[i] && i < n)
+	while (i < n)
 	{
-		for (i; i < n; i++)
-			dest[i] = '\0';
+		dest[i] = '\0';
+		i++;
 	}
 
 	return (dest);
