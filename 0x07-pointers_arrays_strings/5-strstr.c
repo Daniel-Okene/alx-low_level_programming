1a0c3a75319a0f4a98abe2aad84576389c330c1b
Daniel-Okene <simplydaniel11@gmail.com>
2023-03-06 18:21:29 -0800

    solution to task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -18,7 +18,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (i = 0; needle[i]; i++)
 		{
-			if (haystack[i + j] == needle[i])
+			if (haystack[i + j] != needle[i])
 				break;
 		}
 		if (needle[i] == '\0')
