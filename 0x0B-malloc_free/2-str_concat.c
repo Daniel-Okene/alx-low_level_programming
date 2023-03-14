a2210bfb74b783a8d702a3b025a1894276a8a366
Daniel-Okene <simplydaniel11@gmail.com>
2023-03-14 11:27:47 -0700

    solution for task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,7 +22,7 @@ char *str_concat(char *s1, char *s2)
 		size2++;
 
 	newptr = malloc(sizeof(char) * (size1 + size2 + 1));
-	if (newptr == NUL)
+	if (newptr == NULL)
 		return (NULL);
 	for (i = 0; i < (size1 + size2); i++)
 	{
