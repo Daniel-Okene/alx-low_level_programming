e6a068982c762cbeea0c68759d0786b98ca16bf9
Daniel-Okene <simplydaniel11@gmail.com>
2023-03-14 17:15:08 -0700

    added few changes

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,25 +14,15 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, size1 = 0, size2 = 0;
 	char *newptr;
 
-	if (s1 == NULL)
-		size1 = 0;
-	else
-	{
-		for (i = 0; s1[i]; i++)
-			size1++;
-	}
-	if (s2 == NULL)
-		size2 = 0;
-	else
-	{
-		for (i = 0; s2[i]; i++)
-			size2++;
-	}
+	for (i = 0; s1 && s1[i]; i++)
+		size1++;
+	for (i = 0; s2 && s2[i]; i++)
+		size2++;
 
 	newptr = malloc(sizeof(char) * (size1 + size2 + 1));
 	if (newptr == NULL)
 		return (NULL);
-	for (i = 0; i < (size1 + size2); i++)
+	for (i = 0; i <= (size1 + size2); i++)
 	{
 		if (i <= size1)
 			newptr[i] = s1[i];
