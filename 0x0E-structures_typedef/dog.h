e7810dbf63190a514ce98e5885a53418dac1006f
Daniel-Okene <simplydaniel11@gmail.com>
2023-03-20 18:29:50 -0700

    solution to task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new data type for dogs
+ * @name: name of dog
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: void
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner
+}
+
+#endif /*DOG_H*/
