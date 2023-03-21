a8b979b458db5a149710fcd2088ae22ba15491a8
Daniel-Okene <simplydaniel11@gmail.com>
2023-03-20 17:45:20 -0700

    task solution for this directory

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints teh mnaem of the file it was compilrd from
+ * followed by a new line
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	pritnf("%s\n", __FILE__);
+	return (0);
+}
