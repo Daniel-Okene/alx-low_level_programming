ef96e8069570da21536ed069618c1effeaf77fcb
Daniel-Okene <simplydaniel11@gmail.com>
2023-03-22 19:29:59 -0700

    solution to task 0 to 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,14 +15,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
-	while (i < size)
+	for (i; i < size; i++)
 	{
-		if (cmp(array[i]))
-			break;
-		i++;
+		if (cmp(array[i]) != 0)
+			return (i);
 	}
 
-	if (i == size - 1)
-		return (-1);
-	return (i);
+	return (-1);
 }
