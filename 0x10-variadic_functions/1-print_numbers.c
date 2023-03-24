812509d3695d3ada391e5bbc4f1e32e0d9ef56dc
Daniel-Okene <simplydaniel11@gmail.com>
2023-03-23 17:34:13 -0700

    solution to task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers -  prints numbers, followed by a new line
+ * @separator: separator to use
+ * @n: number of variable argument
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+			printf("%d", va_arg(ap, int));
+		else
+		{
+			if (!separator)
+				printf("%d", va_arg(ap, int));
+			else
+				printf("%d%s ", va_arg(ap, int), separator);
+		}
+	}
+
+	va_end(ap);
+	printf("\n");
+}
