fe2f2a61225e6b8661b50b8de6af675702a5c2b9
Daniel-Okene <simplydaniel11@gmail.com>
2023-03-30 18:05:42 -0700

    solution for task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - counts number of elements in a linked list
+ * @h: linked list
+ *
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	if (!h)
+		return (0);
+
+	while(h)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
