c5f8d74c35f18ddf9a8735f00adc40395f3706d1
Daniel-Okene <simplydaniel11@gmail.com>
2023-03-30 18:41:02 -0700

    made changes to task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,17 +12,19 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
 
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	new_node->len = sizeof(str) - 1;
-	new_node->next = *head;
-	*head = new_node;
-	new_node = NULL;
+	new_node->len = len;
+	new_node->next = (*head);
+	(*head) = new_node;
 
-	free(new_node);
 	return (*head);
 }
