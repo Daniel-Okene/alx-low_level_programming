e381c41f61a1466c9e968cc72e916f4b6342c720
Daniel-Okene <simplydaniel11@gmail.com>
2023-04-04 18:33:53 -0700

    solution for task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum of all the data (n) of a linked list
+ * @head: pointer to the first node
+ *
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint *temp = head;
+
+	if (!head)
+		return (0);
+
+	while (ptr)
+	{
+		sum += ptr->n;
+		ptr = ptr->next;
+	}
+
+	return (sum);
+}
