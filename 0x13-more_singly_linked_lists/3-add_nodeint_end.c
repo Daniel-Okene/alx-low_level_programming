4792ed871c963cabeea940e0fe704af2d5ad72bd
Daniel-Okene <simplydaniel11@gmail.com>
2023-04-04 17:43:14 -0700

    corrected typo error

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -23,7 +23,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = new_node;
 	else
 	{
-		while (termp->next)
+		while (temp->next)
 			temp = temp->next;
 
 		temp->next = new_node;
---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+
+/**
+ * free_listint -  frees a linked list
+ * @head: pointer to the first node
+ */
+void free_listint(listint_t *head)
+{
