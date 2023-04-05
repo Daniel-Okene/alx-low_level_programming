7bc6549fcec8728d0c2e7cb959bbfedaa0fa65b7
Daniel-Okene <simplydaniel11@gmail.com>
2023-04-04 17:56:18 -0700

    solution for task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: double pointer to the first node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = *head->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
+}
