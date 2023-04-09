c638726bd84cf844bd781724b8f1f79478b1b1f2
Daniel-Okene <simplydaniel11@gmail.com>
2023-04-09 09:36:56 -0700

    fixed typo error in task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (1);
 	}
-	fd = open(filename, 0_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
 	if (!fd)
 		return (-1);
 
