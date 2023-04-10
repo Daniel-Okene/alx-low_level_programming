e6de146573f8923350679541c0c30b0b07223ddd
Daniel-Okene <simplydaniel11@gmail.com>
2023-04-09 19:16:56 -0700

    solution for task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,11 +15,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (!text_content)
-	{
-		fd = open(filename, O_CREAT, 0600);
-		close(fd);
-		return (1);
-	}
+		text_content = "";
+
 	fd = open(filename, O_CREAT | O_WRONLY, 0600);
 	if (fd < 0)
 		return (-1);
