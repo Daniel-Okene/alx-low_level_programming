5021a63e9d30c3567f45e9f52b2c7b504a216ae5
Daniel-Okene <simplydaniel11@gmail.com>
2023-04-10 00:33:25 -0700

    fixed betty error

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,7 +4,7 @@
  * get_bit - returns the value of a bit at a given index
  * @n: number to convert to binary
  * @index: index to find
- * 
+ *
  * Return: value of index searched
  */
 int get_bit(unsigned long int n, unsigned int index)
