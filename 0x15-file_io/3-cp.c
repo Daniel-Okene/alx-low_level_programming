41cef98ab594924dd074336a2fdfeb62bf8c9724
Daniel-Okene <simplydaniel11@gmail.com>
2023-04-09 20:01:04 -0700

    solution to task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,29 @@
 #include "main.h"
 
 /**
+ * error_code - checks for the corresponding error
+ * @file_from: file from
+ * @file_to: file to
+ * @argv: arguments vector
+ *
+ * Return: void
+ */
+void error_code(int file_from, int file_to, char **argv)
+{
+	if (file_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+
+/**
  * main - copies the content of a file to another file
  * @argc: count of argv
  * @argv: pointer to an array of pointer
@@ -9,27 +32,14 @@
  */
 int main(int argc, char **argv)
 {
-	int count = 0, rfd, wfd, fd1, fd2;
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
 	char buf[1024];
 
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: ./cp <file_from> <file_to>\n");
 		return (97);
-	}
-
-	fd1 = open(argv[1], O_RDONLY);
-	if (fd1 < 0)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		return (98);
-	}
-
-	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd2 < 0)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		return (99);
 	}
 
 	rfd = read(fd1, buf, 1024);
@@ -39,22 +49,30 @@ int main(int argc, char **argv)
 		return (98);
 	}
 
-	while (buf[count] != '\0')
-		count++;
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], 0_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0644);
+	error_code(file_from, file_to, argv);
 
-	wfd = write(fd2, buf, count);
-	if (wfd < 0)
+	nchars = 1024;
+	while (nchars == 1024)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		return (99);
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_code(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_code(0, -1, argv);
 	}
 
-	if (close(fd1))
+	err_close = close(file_from);
+	if (err_close == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fd1);
 		return (100);
 	}
-	if (close(fd2))
+
+	err_close = close(file_to)
+	if (err_close == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fd2);
 		return (100);
